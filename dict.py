c992eb7e9d1b80c74cbfb453876d24dd8ebf8684
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-07-22 23:40:12 +0500

    Dictionary

---
+++ dict.py
@@ -0,0 +1,31 @@
+##dictionary in python          
+
+
+#how to create dictionary 
+
+person ={
+    "Name":"Zuhaib",
+    "Age" :20 ,
+    "City":"Faisalabad",
+    "Email":"zuhaibzafar589@gmail.com"
+}
+
+#printing full dictionary
+
+print(person)
+
+# Accessign Single Elements in dictionary
+print(person["Age"])
+
+#Adding Elements in Dictionary
+person["Last_Name"] ="Zafar"
+
+print(person)
+
+#Changing Value in dictinary
+#remember KEY can't be duplicate in Dictionary vice versa (Value can be )
+
+
+person["Name"] ="Ammar"
+
+print(person)
