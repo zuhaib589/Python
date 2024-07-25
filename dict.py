d2cbad1e6fe51ac311391f69a31210ffd05881e4
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-07-26 00:02:11 +0500

    Dictionary

--- dict.py
+++ dict.py
@@ -3,29 +3,52 @@
 
 #how to create dictionary 
 
-person ={
-    "Name":"Zuhaib",
-    "Age" :20 ,
-    "City":"Faisalabad",
-    "Email":"zuhaibzafar589@gmail.com"
-}
+# person ={
+#     "Name":"Zuhaib",
+#     "Age" :20 ,
+#     "City":"Faisalabad",
+#     "Email":"zuhaibzafar589@gmail.com"
+# }
 
 #printing full dictionary
 
-print(person)
+# print(person)
 
 # Accessign Single Elements in dictionary
-print(person["Age"])
-
+# print(person["Age"])
+# 
 #Adding Elements in Dictionary
-person["Last_Name"] ="Zafar"
+# person["Last_Name"] ="Zafar"
 
-print(person)
+# print(person)
 
 #Changing Value in dictinary
 #remember KEY can't be duplicate in Dictionary vice versa (Value can be )
 
 
-person["Name"] ="Ammar"
+# person["Name"] ="Ammar"
 
-print(person)
+# print(person)
+
+
+#Nested Dictionary 
+
+person ={
+    "Name":"Zuhaib",
+    "Age" :20 ,
+    "City":"Faisalabad",
+    "Email":"zuhaibzafar589@gmail.com",
+    "location":{
+        "city": "Faisalabad",
+        "state": "Punjab"
+    }
+}
+# print(person['location']["city"]) #Faisalabad
+
+#using loop 
+
+for i in person :
+ print(i, person[i])
+ if( type(person[i])== type({})) :
+  for j in person[i]:
+   print(j,person[i][j]) 
--- index.py
+++ index.py
@@ -1,3 +1,30 @@
-set1= {1,2,3,4,5} 
-set2={1,2,3,4}
-print(set1.issuperset(set2))
+
+# x= int(input())
+# print(x + 3)
+# x=9
+# y=8
+# z=3
+ 
+# # print(y + x + z)
+# x=9//2
+# y=9/2
+# z=9%2
+# print(x) #integer
+# print(y) #float
+# print(z) #reminder
+
+# #power 
+# y=2**3
+# print(y)
+
+# x=input("Write your name ")
+# y=x
+# print(y)
+
+# y=8
+# z=2
+# print(8%2)
+
+# x=2
+# x =3
+# print(x)
