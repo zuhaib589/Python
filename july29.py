3fcbbdb0184409de96c8af962f9e4531c3efff3b
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-07-30 23:07:53 +0500

    new

---
+++ july29.py
@@ -0,0 +1,26 @@
+#membership operator 
+
+# x="my book"
+# print("g" in x ) #false 
+# print("g" not in x ) #true
+
+#  identiy operators
+
+# x="my book"
+# y="yours book"
+# z="my book"
+# print(x is y)
+# print(x is z)
+# print(x == z)
+
+# print(id(x))
+# print(id(y))
+# print(id(z))
+
+# x=(1,2,3)
+# y=(1,2,3)
+# print(x is y)
+
+x=5
+y=5
+print(x is y)
