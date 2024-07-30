1dd203e9d19defd488d21d54b8d978cb5ef05564
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-07-31 00:00:51 +0500

    String

---
+++ 30july.py
@@ -0,0 +1,34 @@
+# a=10
+# b=10
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+#---string ---
+
+# a= ''' i hope youre doing this for a resaon where we
+# ash waysdah asdhda '''
+# print(a)
+
+#concationation
+# a="zuhaib"
+# b="zafar"
+# print(a+b)
+# print(a + " " +b)
+# print(len(b))
+
+
+# #---string index ---
+# a="zuhaib"
+# print(a[2])
+# print(len(a))
+# print(a[len(a)-2])
+
+
+#---Slicing String
+
+a="zuhaib"
+# print(a[0:len(a)-1]) #[start(included) :end(not included) ]
+# b=a[len(a)-1] #len(a)=6 [-1]=5 at index 5 "b" is present 
+# print(b)
+print(a[-4:2])
