9dd006a8189dba017be9f646bd237e1d42bac276
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-15 00:01:01 +0500

    dictionary

---
+++ 14aug.py
@@ -0,0 +1,66 @@
+# dictionary 
+
+# dict={
+#     "name":"Zuhaib",
+#      "Age" : 20,
+#     "Class":"BSCS-5",
+#     "City":"Faisalabad",
+#     "Country":"Pakistan",
+    
+# }
+# print(dict)
+# print(len(dict))
+
+# Data Types
+# dict={
+#     (1,0):"Zuhaib",
+#      "age": 20,
+#     "Class":"BSCS-5",
+#     "City":"Faisalabad",
+#     "Country":"Pakistan",   
+# }
+
+# print(dict)
+
+# accessing elements
+# dict={
+#    (1,0):"Zuhaib",
+#     "age": 20,
+#    "Class":"BSCS-5",
+#    "City":"Faisalabad",
+#    "Country":"Pakistan", 
+#    "age":19  
+# }
+# print(dict)
+# print(dict["age"])
+
+
+# Adding new elements 
+
+# dict={
+#    (1,0):"Zuhaib",
+#     "age": 20,
+#    "Class":"BSCS-5",
+#    "City":"Faisalabad",
+#    "Country":"Pakistan", 
+#    "age":19  
+# }
+# dict["section"]="B"
+# print(dict)
+
+
+# change values
+# dict={
+#    (1,0):"Zuhaib",
+#     "age": 20,
+#    "Class":"BSCS-5",
+#    "City":"Faisalabad",
+#    "Country":"Pakistan", 
+#    "age":19  
+# }
+# dict["Class"]="B"
+# print(dict)
+
+# nested list/dictionary
+# list=[[1,2,3],[2.0,4.5],["hello","zuhaib"]]
+# print(list[1][1])
