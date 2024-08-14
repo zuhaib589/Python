f68a9e76b6eac620b7ee5d47131b8bfac79d1efb
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-14 23:05:25 +0500

    sets

---
+++ 13aug.py
@@ -0,0 +1,58 @@
+# SETS
+# unordered,unmuteable,no indexing.don't allow duplicates 
+
+# set1={"zuhaib",7,3,4,True,False,'hello',7.0} #True or 1 , False or 0 
+# print(len(set1))
+# print(type(set1))
+
+# Access items 
+
+# if "zuhaib" in set1:
+#  print(True)
+
+#  print("zuhaib" in set1)
+
+#  Add in Sets add method 
+# set1.add("zafar")
+# print(set1)
+# print(len(set1))
+
+#to add set1 in set2 or update value 
+# set2={1,2,4,5,"hell0"}
+# set3={12,13,15,"zuhaib"}
+# set4= set2 + set3 #will not work
+# print(set4) 
+# set2.update(set3)
+# print(set2)
+# set2.update("qasim ")
+# print(set2)
+
+
+
+
+# remove items in sets
+# set2={1,2,4,5,"hell0"}
+# set2.remove(1) #If we give any item that is not in set it will give error
+# print(set2)
+# set2.discard("he00") #If we give any item that is not in set it will not give error
+# print(set2)
+
+
+
+#clear and del 
+# set2={1,2,4,5,"hell0"}
+# set2.clear() #will remove all items 
+# print(set2)
+# del set2
+# print(set2) #will del full set nothing to show
+
+
+#join sets intersection and union |(pip sign) operator
+set2={1,2,4,5,"hell0"}
+set3={"zuhaib",2,3,4}
+uni=set2.union(set3)
+print(uni)
+
+inter=set2.intersection(set3)
+print(inter)
+
