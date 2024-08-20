1a3e9cc97f58ec7a4bb266befb73cc7e4f084a90
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-21 00:15:54 +0500

    Loops

---
+++ 20aug.py
@@ -0,0 +1,32 @@
+# # increment and decrement
+# a=9
+# ++a
+# # a=--a
+# print(a)
+# print("hello")
+
+# loops
+# while loop "it will execute till our given condition get incorrect or false"
+# a=5
+# while a>=1:
+#     print("X"*a)     "For reverse triangle"
+#     a-=1
+
+# a=1
+# while a <=5:
+#     # print("X"*a)       "For reverse triangle"
+#     a+=1
+
+
+
+# pass in loops will pass the 
+
+# Example with a for loop
+# for i in range(5):
+#     if i == 3:
+#         pass  # Do nothing when i is 3
+# print(f"Processing {i}")
+
+
+#breal statement
+# to stp our loop when we get out answer
