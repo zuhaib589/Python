3bb7772b64da9076b4506d873d6aa40f04336f65
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-22 18:03:36 +0500

    list methods

---
+++ list.py
@@ -0,0 +1,56 @@
+# list methods 
+
+
+# list=["zuhaib",'ammar',"wasiq", 'ammar']
+# print(list)
+
+# # addiing element at last index
+
+# list.append("zafar")
+# print(list)
+
+# #for clearing list items/elements
+# # list.clear()
+# # print(list)
+
+# # for copying same list 
+# a=list.copy()
+# print(a)
+
+# #for counting how many time one element occurs
+# print(list.count('ammar'))
+
+# #will tell 1st occurenece 
+# print(list.index("ammar"))
+
+# #will add at specific index
+# list.insert(3,"akbar")
+# print(list)
+
+# #remove last element by default,if index given then from it
+# # list.pop()
+# # print(list)
+# # list.pop(3)
+# # print(list)
+
+# #remove item with specified values
+# list.remove("zuhaib")
+# print(list)
+
+# #will reverse the list
+# list.reverse()
+# print(list)
+
+# #will sort the list alphabatically/numerically
+
+# #sort the existing list
+# list.sort()
+# print(list)
+
+# #return new list
+# a=sorted(list)
+# print(a)
+
+
+a="head"
+print(a)
