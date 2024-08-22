49c139a69801bda3e644d0bf00b93c390ffa2808
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-23 00:42:56 +0500

    basic function

--- 21aug.py
+++ 21aug.py
@@ -1,4 +1,33 @@
 # foor loop 
-list=["a","b","c"]
+# list=["a","b","c"]
+# for i in list:
+#     print(i)
+
+
+
+# range fucntion/method in for loop 
+# used to get index and value as we
+# ll 
+
+# list=["a","b","c"]
+# for i in range(len(list)):
+#     print(i, list[i])
+
+list=["b","c","a","z","g"]
+target="a"
 for i in list:
-    print(i)
+    if i == target:
+        print("found")
+        continue
+    else:   
+        print("not found")
+
+
+# functions
+
+def sum(a,b):
+    return a+b
+
+print(sum(2,2))
+
+     
