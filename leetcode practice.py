a9b24ee5fd668bfdbb1a0476cbf629fe841d9b22
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-22 23:03:10 +0500

    leetcode practice

---
+++ leetcode practice.py
@@ -0,0 +1,18 @@
+# nums = [0,2,1,5,3,4,]
+# # nums.sort()
+# # print(nums)
+# ans=[]
+# for i in range(len(nums)):
+# #  x=[i,nums[i]]
+#  print(i,nums[i])
+# #  desired_ans=nums[x]
+# #  print(desired_ans)
+
+
+# nums = [0,2,1,5,3,4]
+# a=nums.copy()
+# ans=a+nums
+# print(ans)
+# for i in nums:
+# #  print(i,nums[i])
+#  print(nums.index(i))
