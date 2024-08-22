0f7a08bc687efb1e3cb0c5797771375e4ac94a4b
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-22 23:02:50 +0500

    Ass_3

---
+++ Ass_3.py
@@ -0,0 +1,20 @@
+# Assignment: Practice with Logical Operators in Conditions
+
+
+
+# 1. Question 1: Write a program that accepts a user’s age, citizenship status, and criminal 
+# record status. Determine if the user is eligible to vote. A person is eligible to vote
+# if they are at least 18 years old, are a citizen of the country, and do not have a criminal
+# record. Use logical operators to check all these conditions in a single statement.
+
+'''Age=int(input("Write your age: "))
+Country=input("Write you country: ")
+C_Record=input("Do you have criminal record ? (yes / no): ")
+
+print("You're eligible to vote") if Age >= 18 and Country =="pakistan" and C_Record == "no" else print("You can't vote")'''
+  
+ 
+# 2. Question 2: Create a program that takes three integers as input and determines whether 
+# they can form an arithmetic progression. The numbers form an arithmetic progression if 
+# the difference between consecutive numbers is the same. Use logical operators to validate
+# this condition and print whether the numbers form a valid sequence.  
