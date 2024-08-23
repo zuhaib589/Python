c2939aa00c9fd97cc223bccabc7ceb220c21d3a6
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-24 01:07:19 +0500

    func

--- 21aug.py
+++ 21aug.py
@@ -13,21 +13,20 @@
 # for i in range(len(list)):
 #     print(i, list[i])
 
-list=["b","c","a","z","g"]
-target="a"
-for i in list:
-    if i == target:
-        print("found")
-        continue
-    else:   
-        print("not found")
+# list=["b","c","a","z","g"]
+# target="a"
+# for i in list:
+#     if i == target:
+#         continue
+#     print(i)
+
 
 
 # functions
 
-def sum(a,b):
-    return a+b
+# def sum(a,b):
+#     return a+b
 
-print(sum(2,2))
+# print(sum(2,2))
 
      
