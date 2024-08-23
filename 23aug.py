0e2149e186c231355116441d1490e1bfa9b16af4
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-24 01:07:40 +0500

    nested loops

---
+++ 23aug.py
@@ -0,0 +1,18 @@
+# nested loops
+# a=[1,2]
+# b=[3,4]
+# for i in a:
+#     for j in b:   
+#      print(i,j)
+    
+# for i in range(2): 
+#     for j in range(2):   
+#      print ("*")
+
+# a="zuhaib"
+# for i in a:
+#     print(i,end="")
+
+n=10
+for i in reversed(range(n)) :
+    print(i)
