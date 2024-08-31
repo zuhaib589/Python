ac6be90c798efa383c95ed5d535e44d672da0bd3
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-31 23:05:39 +0500

    loop ques with functions

--- 30aug.py
+++ 30aug.py
@@ -16,10 +16,39 @@ print(reverse_number(123))
 def table(num):
     a=11
     for i in range(1,a):
-     print(f" {num} x {i} ={num*i}")   
-        
-        
-      
+     print(f" {num} x {i} = {num*i}")   
+     
         
 table(2)
-table(-3)
+table(-3) 
+     
+# 18: Write a program using a for loop to calculate the factorial of a given number.
+
+
+def factorial(n):       
+    ans=1
+    for i in range(1,n+1):
+        ans *= i
+        
+    return ans 
+
+factorial(4)     
+
+
+
+# Problem 16: Create a program that uses a while loop to find the sum of digits of a given positive integer.
+
+def sumofdigit(n):
+    
+    if n < 0 :
+     return print("Sorry digit cannot be negative integer  ")
+
+    total=0
+    while n > 0:
+       total= total + n % 10     
+       n=n//10
+    print(total)    
+      
+      
+        
+sumofdigit(18)      
