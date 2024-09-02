e3e9dbe60abae5bdf1580700784641afbc527f34
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-02 22:49:19 +0500

    list problems

--- practice.py
+++ practice.py
@@ -14,3 +14,31 @@ def squared_tuple(n):
 
 num=(1,2,3,4,5,6,7,8,9)
 squared_tuple(num)
+
+#  12: Create a function that takes a tuple and an element,
+# and returns a new tuple with the element added to the end of the tuple.
+
+
+def tupl_plus_ele(tup,ele):
+    print (tup + ele)
+    
+    
+t1=(1,2,3,4)
+ele=6,  
+tupl_plus_ele(t1,ele)   
+
+
+
+#  6: Write a program that takes a list of integers and returns the list sorted in ascending order.
+
+l1=[1,2,4,6,5,3,0,-1]
+
+def sort_list(n):
+    n.sort()
+    return n
+    
+  
+print(sort_list(l1) )
+    
+    
+    
