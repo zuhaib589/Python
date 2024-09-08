73340b2435c57eb0085495fbd90a141d8d2b3778
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-08 20:57:50 +0500

    practice

--- leetcode practice.py
+++ leetcode practice.py
@@ -25,3 +25,13 @@
 #greater2 = max(nums)
 #product = (greatest-1)* (greater2 - 1)
 #return product
+
+nums = [0,1,0,3,12]
+arr=[]
+arr1=[]
+for i in range(len(nums)):
+    if nums[i] == 0:
+     arr.append(nums[i])
+    else:
+     arr1.append(nums[i])
+print (arr1 + arr)
