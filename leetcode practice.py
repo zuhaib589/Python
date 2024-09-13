bb2022d74fd1b54f2761461f6f6bdec578482369
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-13 22:59:11 +0500

    practice 1662

--- leetcode practice.py
+++ leetcode practice.py
@@ -26,12 +26,38 @@
 #product = (greatest-1)* (greater2 - 1)
 #return product
 
-nums = [0,1,0,3,12]
-arr=[]
-arr1=[]
-for i in range(len(nums)):
-    if nums[i] == 0:
-     arr.append(nums[i])
-    else:
-     arr1.append(nums[i])
-print (arr1 + arr)
+# nums = [0,1,0,3,12]
+# arr=[]
+# arr1=[]
+# for i in range(len(nums)):
+#     if nums[i] == 0:
+#      arr.append(nums[i])
+#     else:
+#      arr1.append(nums[i])
+# print (arr1 + arr)
+
+
+# 1662:
+# https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+# word1 = ["ab", "c"]
+# word2 = ["a", "bc"]
+# a=""
+# b=""
+# for i in word1:
+#     if i not in a:
+#         a +=i
+
+# for j in word2 :
+#    if j not in b:
+#        b += j 
+# print(a == b )
+# print(a ,b )
+
+a=""
+b=""
+for i in word1:
+    a +=i    
+for j in word2 :
+    b += j 
+print (a ==b)
