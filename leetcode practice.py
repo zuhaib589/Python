84bea3ea50ad1bda6c04d11859f1a2c9b3d3651d
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-14 22:33:48 +0500

    2215

--- leetcode practice.py
+++ leetcode practice.py
@@ -54,10 +54,43 @@
 # print(a == b )
 # print(a ,b )
 
-a=""
-b=""
-for i in word1:
-    a +=i    
-for j in word2 :
-    b += j 
-print (a==b)
+# sol 2 
+
+# a=""
+# b=""
+# for i in word1:
+#     a +=i    
+# for j in word2 :
+#     b += j 
+# print (a==b)
+
+
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/
+# 2215
+# class Solution:
+#     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+#             ans1=[]
+#             ans2=[]
+#             main=[]
+#             for i in range(len(nums1)):
+#                 if nums1[i] not in nums2 and nums1[i] not in ans1:
+#                     ans1.append(nums1[i])
+#             main.append(ans1)
+#             # print(main)
+#             for j in range(len(nums2)):
+#                 if nums2[j] not in nums1 and nums2[j] not in ans2:
+#                     ans2.append(nums2[j])
+#             main.append(ans2)
+#             return main
+
+# sol 2
+
+# set1 = set(nums1)
+# set2 = set(nums2)
+    
+#     # Find the difference between the sets
+# diff1 = list(set1 - set2)  # Elements in nums1 but not in nums2
+# diff2 = list(set2 - set1)  # Elements in nums2 but not in nums1
+    
+#     # Return the result as a list of two lists
+# print( [diff1, diff2])
