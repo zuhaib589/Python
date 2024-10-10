2810dd88c7b2382d4b4d4051bc4f910b5cc6db34
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-10-10 10:42:31 +0500

    .task1

---
+++ task.py
@@ -0,0 +1,18 @@
+# a=int(input("Enter 1st number "))
+# b=int(input("Enter 2st number "))
+# c=int(input("Enter 3st number "))
+
+# if a >b and a > c:
+#     print(f'{a} is greater than {b} and {c} ')
+# elif b> a and b>c :    
+#         print(f'{b} is greater than {a} and {c} ')
+# else:
+#      print(f'{c} is greater than {a} and {b} ')
+
+edx=1
+ebx=100
+count=0
+while edx < ebx:
+    edx += 1
+    count +=1
+print(count)    
